9154ff5db29c30bed7a24bd99859e6d12a7f03fa
goliath309 <roll.93@hotmail.com>
2015-05-21 19:33:00 -0500

    Ya hace las consultas correctamente y las guarda

--- consulta.php
+++ consulta.php
@@ -4,7 +4,7 @@
 	$conexion=conectar();	
 	
 		
-	$usuario=$_SESSION['usuario'];	
+	$id_usuario=$_SESSION['usuario'];	
 	$area = $_SESSION['area'];
 	$descripcion = $_SESSION['descripcion'];
 	$especialidad = $_POST['especialidad'];
@@ -19,17 +19,80 @@
 	$campo = mysql_fetch_row($resultado);
 	$id_enlace = $campo[0] + 1;
 	
-	$fecha = date('y.m.d');
-	$hora = time('H.i.s');
-	
-	echo $hora;
+	$fecha = date('d.m.y');
+	$hora= date("H");	
+	$min= date("i");
+	$minn=$min-22;
+
+if($hora>12)
+{
+	$ampm="pm";
+	$horaa=$hora-12;
+}
+else
+{
+	$ampm="am";
+}
+
+$tiempo = $horaa . ":" . $minn . " " . $ampm;
 
 	if ($id_enlace == "") 
 	{
 		$id_enlace = 1;
 	}
 	
-	$sqla = "INSERT INTO `Enlace` (id_enlace, id_usuario, id_trabajador, descripcion) values ('$id_usuario', '$nombre', '$apellidos', '$correo', '$telefono', '$domicilio', '$password')";
+	$sqla = "INSERT INTO `Enlace` (id_enlace, id_usuario, id_trabajador, descripcion, fecha, hora) values ('$id_enlace', '$id_usuario', '$id_trabajador', '$descripcion', '$fecha', '$tiempo')";
 	$resultadoa = query($sqla, $conexion);
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="shortcut icon" href="images/icono.ico">
+<meta http-equiv="Content-Type" content="text/html"/>
+  <title>Usuario</title>
+  <link rel="stylesheet" type="text/css" href="estiloUsuario.css" />
+</head>
+
+<style type="text/css">
+
+#contenidoo {
+padding: 2em;
+margin: 0 auto;
+margin-top: 20em;
+border-radius: 25px;
+ background-color: #FFFFFF;
+  border: solid 0.1em #000000;
+  overflow: hidden;
+  width: 30em;
+  height: 20em;
+  box-sizing:border-box;
+}
+
+#centrar
+{
+	text-align: center;
+	
+}
+
+</style>
+
+<body>
+<div id="contenidoo">
+<div id="centrar">
+<h2>
+
+	La consulta se ha enviado exitosamente, recibir&aacute; su respuesta en breve.
+
+</h2>
+
+
+<br />
+<br />
+<br />
+<a href="usuario.php"  class="bt">Regresar</a>
+</div>
+</div>	
+<body>
+</html>
