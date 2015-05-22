1a0a9b2ff595e07a973425f27d51c51f17f08298
goliath309 <roll.93@hotmail.com>
2015-05-21 21:59:41 -0500

    Usuario terminado con cerrar sesion

---
+++ cerrarsesion.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="shortcut icon" href="images/icono.ico">
+<meta http-equiv="Content-Type" content="text/html"/>
+  <title>Usuario</title>
+  <link rel="stylesheet" type="text/css" href="estiloUsuario.css" />
+</head>
+
+<style type="text/css">
+
+#contenidoo {
+padding: 2em;
+margin: 0 auto;
+margin-top: 20em;
+border-radius: 25px;
+ background-color: #FFFFFF;
+  border: solid 0.1em #000000;
+  overflow: hidden;
+  width: 30em;
+  height: 20em;
+  box-sizing:border-box;
+}
+
+#centrar
+{
+	text-align: center;
+	
+}
+
+</style>
+
+<body>
+<div id="contenidoo">
+<div id="centrar">
+<h2>
+
+	Usted ha cerrado sesion.
+
+</h2>
+
+
+<br />
+<br />
+<br />
+<a href="index.html"  class="bt">OK</a>
+</div>
+</div>	
+<body>
